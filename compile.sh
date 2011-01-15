8f6cc62242b155f5da99cbbf6abbb8b65fef9d37
jbeard4 <jbeard4@29373c21-0b15-0410-bdb5-f8d6ddd024e2>
2011-01-15 02:31:28 +0000

    Moved non-printable movement characters to parallel regions.
    git-svn-id: https://msdl.cs.mcgill.ca/svn/AToM3/sandbox/jbeard4@297 29373c21-0b15-0410-bdb5-f8d6ddd024e2

--- compile.sh
+++ compile.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-xsltproc editor-behaviour-macros.xsl editor-behaviour.xml > /tmp/out.scxml
-/home/jacob/workspace/gsoc2010/scxml-js/py-run.sh --backend state /tmp/out.scxml > out.js
+/home/jacob/workspace/gsoc2010/scxml-js/py-run.sh --backend state editor-behaviour.xml > out.js
